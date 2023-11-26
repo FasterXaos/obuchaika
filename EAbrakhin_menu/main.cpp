4a5059107bd739192f5fb413054bb6e3e1fb8e3a
Egor Abrakhin <fasterx7as@gmail.com>
2023-11-26 16:53:17 +0300

    .

--- EAbrakhin_menu/main.cpp
+++ EAbrakhin_menu/main.cpp
@@ -3,43 +3,12 @@
 
 #include "menu.hpp"
 #include "menu_functions.hpp"
+#include "menu_items.hpp"
 
 int main() {
 	std::setlocale(LC_ALL, "");
 
-	EAbrakhin::MenuItem study_summ = { "1 - Складывать", EAbrakhin::study_summ };
-	EAbrakhin::MenuItem study_substract = { "2 - Вычитать", EAbrakhin::study_substract };
-	EAbrakhin::MenuItem study_multiply = { "3 - Умножать", EAbrakhin::study_multiply };
-	EAbrakhin::MenuItem study_divide = { "4 - Делить", EAbrakhin::study_divide };
-	EAbrakhin::MenuItem study_go_back = { "0 - Выйти в меню", EAbrakhin::study_go_back };
-
-	EAbrakhin::MenuItem* study_children[] = {
-		&study_go_back,
-		&study_summ,
-		&study_substract,
-		&study_multiply,
-		&study_divide,
-	};
-	const int study_size = sizeof(study_children) / sizeof(study_children[0]);
-
-	EAbrakhin::MenuItem study = { "1 - Математика", EAbrakhin::show_menu, study_children, study_size };
-	EAbrakhin::MenuItem exit = { "0 - Выход", EAbrakhin::exit };
-
-	EAbrakhin::MenuItem* main_children[] = { &exit, &study };
-	const int main_size = sizeof(main_children) / sizeof(main_children[0]);
-
-	EAbrakhin::MenuItem main = { nullptr, EAbrakhin::show_menu, main_children, main_size };
-
-	study_go_back.parent = &study;
-	study_summ.parent = &study;
-	study_substract.parent = &study;
-	study_multiply.parent = &study;
-	study_divide.parent = &study;
-
-	study.parent = &main;
-	exit.parent = &main;
-
-	const EAbrakhin::MenuItem* current = &main;
+	const EAbrakhin::MenuItem* current = &EAbrakhin::MAIN;
 	do {
 		current = current->func(current);
 	} while (true);
---
+++ EAbrakhin_menu/menu_items.cpp
@@ -0,0 +1,48 @@
+﻿#include "menu_items.hpp"
+#include "menu_functions.hpp"
+
+const EAbrakhin::MenuItem EAbrakhin::STUDY_SUMM = { 
+	"1 - Складывать", EAbrakhin::study_summ, &EAbrakhin::STUDY 
+};
+const EAbrakhin::MenuItem EAbrakhin::STUDY_SUBSTRACT = { 
+	"2 - Вычитать", EAbrakhin::study_substract, &EAbrakhin::STUDY 
+};
+const EAbrakhin::MenuItem EAbrakhin::STUDY_MULTIPLY = { 
+	"3 - Умножать", EAbrakhin::study_multiply, &EAbrakhin::STUDY 
+};
+const EAbrakhin::MenuItem EAbrakhin::STUDY_DIVIDE = { 
+	"4 - Делить", EAbrakhin::study_divide, &EAbrakhin::STUDY 
+};
+const EAbrakhin::MenuItem EAbrakhin::STUDY_GO_BACK = {
+	"0 - Выйти в меню", EAbrakhin::study_go_back, &EAbrakhin::STUDY 
+};
+
+namespace {
+	const EAbrakhin::MenuItem* study_children[] = {
+		&EAbrakhin::STUDY_GO_BACK,
+		&EAbrakhin::STUDY_SUMM,
+		&EAbrakhin::STUDY_SUBSTRACT,
+		&EAbrakhin::STUDY_MULTIPLY,
+		&EAbrakhin::STUDY_DIVIDE,
+	};
+	const int study_size = sizeof(study_children) / sizeof(study_children[0]);
+}
+
+const EAbrakhin::MenuItem EAbrakhin::STUDY = {
+	"1 - Математика", EAbrakhin::show_menu, &EAbrakhin::MAIN, study_children, study_size 
+};
+const EAbrakhin::MenuItem EAbrakhin::EXIT = {
+	"0 - Выход", EAbrakhin::exit, &EAbrakhin::MAIN
+};
+
+namespace {
+	const EAbrakhin::MenuItem* main_children[] = {
+		&EAbrakhin::EXIT,
+		&EAbrakhin::STUDY
+	};
+	const int main_size = sizeof(main_children) / sizeof(main_children[0]);
+}
+
+const EAbrakhin::MenuItem EAbrakhin::MAIN = {
+	nullptr, EAbrakhin::show_menu, nullptr, main_children, main_size 
+};
